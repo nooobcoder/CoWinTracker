0ca308f8092dc4ec2e758260a83809568bbf10a5
Ankur Paul <suryashi2013@gmail.com>
2022-05-17 12:34:31 +0000

    Use createRoot for React v18 correctly.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
